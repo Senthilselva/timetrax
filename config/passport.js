593b259861a026d58b78c3b44bac3aa2b0231c1d
Graciela Diaz <gudiaz@me.com>
2017-01-12 15:14:31 -0500

    updated server

---
+++ config/passport.js
@@ -0,0 +1,50 @@
+var LocalStrategy = require('passport-local').Strategy,
+    bcrypt = require('bcryptjs'),
+    models = require('../models/');
+
+module.exports = function(passport) {
+  
+  passport.use('local', new LocalStrategy(
+    function(username, password, done) {
+      console.log("local Strategy" + username + "  " + password);
+      models.User.findOne({
+        where: {
+          'username': username
+        }
+      }).then(function (user) {
+        console.log(user.password)
+        console.log(password)
+        if (user == null) {
+          return done(null, false, { message: 'Incorrect credentials.' })
+        }
+        
+        var hashedPassword = bcrypt.hashSync(password, user.salt)
+        
+        if (user.password === hashedPassword) {
+          console.log("found user")
+          return done(null, user)
+        }
+        console.log("wrong password")
+        return done(null, false, { message: 'Incorrect credentials.' })
+      })
+    }
+  ))
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function(id, done) {
+    models.User.findOne({
+      where: {
+        'id': id
+      }
+    }).then(function (user) {
+      if (user == null) {
+        done(new Error('Wrong user id.'))
+      }
+      
+      done(null, user)
+    })
+  })
+}
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var express = require("express");
+var session = require('express-session'); 
+var methodOverride = require('method-override'); // for deletes in express
+var debug = require('debug')('express-example');
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser'); // for working with cookies
 var compression = require('compression')
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var path = require('path')
-var app = express();
+var passport = require('passport'); // pass passport for configuration
 var port = process.env.PORT || 3000;
 
+var app = express();
+var setupPassport = require('./config/passport')(passport),
+    flash = require('connect-flash');
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 app.use(compression());
+app.use(methodOverride('_method')) // override POST to have DELETE and PUT
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
@@ -13,6 +27,13 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 //app.use(express.static(path.join(__dirname, 'public')))
 //app.use(express.static(process.cwd() + '/public'));
 app.use(express.static(__dirname + '/public'))
+
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 60000 }}));
+app.use(cookieParser());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
 
 //console.log('going to initialize db');
 //var initdb = require("./db/init.js");
