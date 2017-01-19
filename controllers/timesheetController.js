0100ea34330d55027c3c058b6c44099540d2dd59
Graciela Diaz <gudiaz@me.com>
2017-01-19 18:48:40 -0500

    merge senthil changes

--- controllers/timesheetController.js
+++ controllers/timesheetController.js
@@ -6,7 +6,7 @@ var moment =require('moment');
 
 //enter a new record into the timesheet table with out the end time
 router.post("/create", function(req,res){
-	console.log("inside time sheet");
+  console.log("inside time sheet");
   // console.log(JSON.stringify(req.body));
   var newtimesheet = {};
   newtimesheet.JobId = req.body.JobId;
@@ -18,8 +18,8 @@ router.post("/create", function(req,res){
 
   models.Timesheet.create(newtimesheet).then(function(data){
     console.log("created data ")
-  	//console.log(data)
-  	res.json(data)
+    //console.log(data)
+    res.json(data)
   });
   //res.json(req.body);
 })
@@ -27,22 +27,63 @@ router.post("/create", function(req,res){
 router.post("/update", function(req,res){
   console.log("inside time sheet update");
   console.log(JSON.stringify(req.body));
-  var userId= req.body.userId;
-  var jobId = req.body.jobId;
+  
+  var cardId = req.body.cardId;
   var clockOut = new Date(req.body.clockOut);
 
-  models.Timesheet.update({
-    clockedOut : clockOut
-  }, {
-    where: {
-      JobId : jobId,
-      UserId : userId
-      }
-  }).then(function(data,err){
+    models.Timesheet.update({
+      clockedOut : clockOut
+    }, {
+      where: {
+       id : cardId
+        }
+    }).then(function(data,err){
     console.log("updated")
-    console.log(JSON.stringify(data))
+    console.log(JSON.stringify(data));
+      res.json(data)
   })
 
   });
+
+router.get('/user/:userName', function(req,res) {
+  console.log("inside user Timesheets");
+  console.log(JSON.stringify(req.params));
+  //console.log(req);
+
+    models.Timesheet.findAll(
+
+      { include: [
+        { 
+          model : models.User,
+          where: { username: req.params.userName} 
+        },
+        {
+          model:  models.Job 
+        }
+      ]
+    }).then(function(data){
+    var jobList = [];
+    // console.log(JSON.stringify(data[0]));
+
+    for(var i=0; i< data.length; i++){
+    // console.log(data[i].startDate+" "+ 
+    //             data[i].startDate+" "+
+    //             data[i].Job.name);
+    var job = {};
+    job.id = data[i].id;
+    job.clockedInDate = moment(data[i].clockedInDate).format('L');
+    job.clockIn = data[i].clockedIn;
+    job.clockOut = data[i].clockedOut;
+    job.jobName = data[i].Job.name;
+    
+    console.log(job);
+
+    jobList.push(job)
+  }
+     res.json(jobList)
+  })
+
+});
+
 
 module.exports = router;
